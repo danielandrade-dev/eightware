80a8f3b26a20c99cfa49296f9e2cab2f667bc0b2
Daniel Andrade <danielthooth@gmail.com>
2025-04-23 14:01:56 -0300

    Remove a validação de unicidade do email no modelo de usuário, mantendo apenas a presença e o formato. Isso simplifica as validações de entrada para o registro de usuários.

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
 
   # Validações
   validates :email, presence: true, 
-                   uniqueness: true, 
                    format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true,
                       length: { minimum: 8 },
